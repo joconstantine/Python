4ef02678be09ff553b6d64c6bb99caeaf1fdfb4e
joconstantine <joconstantine@users.noreply.github.com>
2021-04-05 23:14:16 +0800

    05/04/2021 23:!4

---
+++ oop/demo.py
@@ -0,0 +1,13 @@
+a_string = "this is\na string split\t\t and tabbed"
+print(a_string)
+
+raw_string = r"this is\na string split\t\t and tabbed"
+print(raw_string)
+
+b_string = "this is" + chr(10) + "a string split" + chr(9) + chr(9) + "and tabbed"
+print(b_string)
+
+backslash_string = "this is a backslash \\followed by some text"
+print(backslash_string)
+
+error_string = r"this string ends with \\"
