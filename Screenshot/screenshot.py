a92a42fb0f6c3b6ca097fb781029a119ef5e1493
joconstantine <joconstantine@users.noreply.github.com>
2021-05-04 23:37:43 +0800

    04/05/2021 23:37

--- Screenshot/screenshot.py
+++ Screenshot/screenshot.py
@@ -9,6 +9,7 @@ try:
 except ImportError:
     print("PIL is not installed,")
     "Install using: pip install PIL"
+    sys.exit(1)
 import tkinter as tk
 
 VIEWPORT_SIZE = None
@@ -22,8 +23,8 @@ def main(url, w, h):
     VIEWPORT_SIZE = (w, h)
     SCREENSHOT_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)),
                                    "SCREENSHOT.png")  # current working dir
-    check_version()
-    sys.excepthook = cef.ExceptHook()  # to shutdown all the processes while errors
+    check_versions()
+    sys.excepthook = cef.ExceptHook  # to shutdown all the processes while errors
 
     if os.path.exists(SCREENSHOT_PATH):
         print("Remove old screenshot")
@@ -90,7 +91,79 @@ def command_line_arguments():
 def create_browser(settings):
     global VIEWPORT_SIZE, URL
     parent_window_handle = 0
+    window_info = cef.WindowInfo()
+    window_info.SetAsOffscreen(parent_window_handle)
+    print("Viewport size: {size}".format(size=str(VIEWPORT_SIZE)))
+    print("Loading URL: {url}".format(url=URL))
+    browser = cef.CreateBrowserSync(window_info=window_info, settings=settings, url=URL)
+    browser.SetClientHandler(LoadHandler())
+    browser.SetClientHandler(RenderHandler())
+    browser.SendFocusEvent(True)
+    browser.WasResized()
 
+
+def save_screenshot(browser):
+    global SCREENSHOT_PATH
+    buffer_string = browser.GetUserData("OnPaint.buffer string")
+    if not buffer_string:
+        raise Exception("buffer_string is empty because"
+                        " OnPaint never called?")
+    image = Image.frombytes("RGBA", VIEWPORT_SIZE, buffer_string, "raw", "RGBA", 0, 1)
+    image.save(SCREENSHOT_PATH, "PNG")
+    print("Saved screenshot to: {path}".format(path=SCREENSHOT_PATH))
+
+
+def open_with_default_application(path):
+    if sys.platform.startswith("darwin"):   # Mac operating system
+        subprocess.call(("open", path))
+    elif os.name == "nt":
+        os.startfile(path)
+    elif os.name == '"posix':
+        subprocess.call(("xdg-open", path))
+
+
+def exit_app(browser):
+    print("Closing Browser and Exiting Application")
+    browser.CloseBrowser()
+    cef.QuitMessageLoop()
+
+
+class LoadHandler(object):
+    def OnLoadingStateChange(self, browser, is_loading, **_):
+        if not is_loading:
+            sys.stdout.write(os.linesep)
+            print("Website has been loaded")
+            save_screenshot(browser)
+            cef.PostTask(cef.TID_UI, exit_app, browser)
+
+    def OnLoadError(self, browser, frame, error_code, failed_url, **_):
+        if not frame.IsMain():
+            return
+        print("Failed to load URL: {url}".format(url=failed_url))
+        print("Error code: {code}".format(code=error_code))
+        cef.PostTask(cef.TID_UI, exit_app, browser)
+
+
+class RenderHandler(object):
+    def __init__(self):
+        self.OnPaint_called = False
+
+    def GetViewRect(self, rect_out, **_):
+        rect_out.extend([0, 0, VIEWPORT_SIZE[0], VIEWPORT_SIZE[1]])
+        return True
+
+    def OnPaint(self, browser, element_type, paint_buffer, **_):
+        if self.OnPaint_called:
+            sys.stdout.write(".")
+            sys.stdout.flush()
+        else:
+            sys.stdout.write("OnPaint")
+            self.OnPaint_called = True
+        if element_type == cef.PET_VIEW:
+            buffer_string = paint_buffer.GetBytes(mode='rgba', origin='top-left')
+            browser.SetUserData("OnPaint.buffer string", buffer_string)
+        else:
+            raise Exception("Unsupported element type in OnPaint")
 
 
 root = tk.Tk()
@@ -99,7 +172,7 @@ root.geometry("400x200")
 
 class Widgets:
     def __init__(self, label_text, default_value):
-        self.lab = tk.Label(root, textwrap=label_text)
+        self.lab = tk.Label(root, text=label_text)
         self.lab.pack()
         self.v = tk.StringVar()
         self.entry = tk.Entry(root, textvariable=self.v)
@@ -115,3 +188,5 @@ label4 = tk.Label(root, text="                  ")
 label4.pack()
 label5 = tk.Label(root, text="Press the Enter key to create screenshot")
 label5.pack()
+
+root.mainloop()
