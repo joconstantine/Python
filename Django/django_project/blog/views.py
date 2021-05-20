08793581c45a3997de343b6468d5b2922fdff30f
joconstantine <joconstantine@users.noreply.github.com>
2021-05-20 23:23:11 +0800

    20/05/2021 18:00

--- Django/django_project/blog/views.py
+++ Django/django_project/blog/views.py
@@ -1,6 +1,4 @@
-from abc import ABC
-
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
     ListView,
@@ -9,14 +7,15 @@ from django.views.generic import (
     UpdateView,
     DeleteView,
 )
+from django.contrib.auth.models import User
 from .models import Post
 
 
-def home(request):
-    context = {
-        'posts': Post.objects.all(),
-    }
-    return render(request, 'blog/home.html', context)
+# def home(request):
+#     context = {
+#         'posts': Post.objects.all(),
+#     }
+#     return render(request, 'blog/home.html', context)
 
 
 class PostListView(ListView):
@@ -24,6 +23,18 @@ class PostListView(ListView):
     template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']  # - sign to denote descending order
+    paginate_by = 5
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 class PostDetailView(DetailView):
--- Django/django_project/django_project/urls.py
+++ Django/django_project/django_project/urls.py
@@ -25,6 +25,18 @@ urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='log-in'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='log-out'),
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(template_name='users/password-reset.html'),
+         name='password_reset'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='users/password-reset-confirm.html'),
+         name='password_reset_confirm'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='users/password-reset-done.html'),
+         name='password_reset_done'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='users/password-reset-complete.html'),
+         name='password_reset_complete'),
     path('profile/', user_views.profile, name='profile'),
     path('', include('blog.urls'))
 ]
