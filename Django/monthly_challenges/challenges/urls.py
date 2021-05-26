8ae215eb083f32073271605624f2df899c43a8fd
joconstantine <joconstantine@users.noreply.github.com>
2021-05-26 17:13:17 +0800

    26/05/2021 17:13

--- Django/monthly_challenges/challenges/urls.py
+++ Django/monthly_challenges/challenges/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     # path("january", views.january),
     # path("february", views.february),
     # path("march", views.march),
-    path("", views.index),  # /challenges/
+    path("", views.index, name="index"),  # /challenges/
     path("<int:month>", views.month_challenge_by_number),
     path("<str:month>", views.monthly_challenge, name="month-challenge"),
 ]
