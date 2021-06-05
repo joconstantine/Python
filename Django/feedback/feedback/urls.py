54ed9f19f1d08ee60456eca4388c20012242602c
joconstantine <joconstantine@users.noreply.github.com>
2021-06-05 10:09:04 +0800

    05/06/2021

--- Django/feedback/feedback/urls.py
+++ Django/feedback/feedback/urls.py
@@ -13,7 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from feedback.settings import MEDIA_ROOT
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("reviews.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("reviews.urls")),
+    path("profiles/", include("profiles.urls")),
+] + static(settings.MEDIA_URL, document_root=MEDIA_ROOT)
