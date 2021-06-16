2298ce08317ff9dee8a93d020b1740a6900f0963
joconstantine <joconstantine@users.noreply.github.com>
2021-06-16 19:21:49 +0800

    16/06/2021 19:21

--- Django/greatkart/greatkart/urls.py
+++ Django/greatkart/greatkart/urls.py
@@ -22,7 +22,8 @@ from django.urls.conf import include
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('store/', include('store.urls')),
+    path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
+    path("store/", include("store.urls")),
+    path("cart/", include("carts.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
